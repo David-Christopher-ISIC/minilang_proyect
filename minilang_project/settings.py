dbb9727153e29d99c01fe000958d974824ec871b
David <gaber.david.christopher@gmail.com>
2025-10-24 09:50:29 -0600

    Segundo commit

--- minilang_project/settings.py
+++ minilang_project/settings.py
@@ -114,8 +114,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = 'static/'
+import os
+STATIC_URL = '/static/'
 STATICFILES_DIRS = []
 
 # Default primary key field type
@@ -123,5 +123,4 @@ STATICFILES_DIRS = []
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
